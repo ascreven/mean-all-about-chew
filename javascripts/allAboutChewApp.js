5b4a94e6ae165efa2588b5f86e54a7389b841cd8
Alexandra <akscreven@gmail.com>
2016-02-16 12:38:07 -0500

    added basic styling

--- javascripts/allAboutChewApp.js
+++ javascripts/allAboutChewApp.js
@@ -10,3 +10,16 @@ app.controller('mainController', function( $scope ){
     $scope.newPet = {created_by: '', name: '', created_at: ''};
   };
 });
+
+app.controller('authController', function( $scope ){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    $scope.error_message = 'register request for ' + $scope.user.username;
+  }
+})
