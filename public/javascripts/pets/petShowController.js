25417ad4faa45f1b720178e6c4a1982efe27d247
Alexandra <akscreven@gmail.com>
2016-02-29 13:38:14 -0500

    show page is working

--- public/javascripts/pets/petShowController.js
+++ public/javascripts/pets/petShowController.js
@@ -3,13 +3,15 @@
   angular
   .module("pets")
   .controller("PetShowController", [
+    '$scope',
     "PetFactory",
     "$stateParams",
     PetShowControllerFunction
   ]);
 
-  function PetShowControllerFunction(PetFactory, $stateParams){
-    this.pet = PetFactory.get({id: '56c7aea8f438e80f4826e334'});
+  function PetShowControllerFunction($scope, PetFactory, $stateParams){
+    // this.pet = PetFactory.get({id: '56c7aea8f438e80f4826e334'});
+    $scope.pet = PetFactory.get({id: $stateParams.id});
     console.log($stateParams);
     // var pet = PetFactory.get({ id: $scope.id})
   }
